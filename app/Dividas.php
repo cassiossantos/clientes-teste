1a3a6ea02739278ed90c565fa7593e5f05d454b0
cassiossantos <42851217+cassiossantos@users.noreply.github.com>
2018-11-08 13:02:34 -0200

    Update Dividas.php

--- app/Dividas.php
+++ app/Dividas.php
@@ -8,7 +8,7 @@ class Dividas extends Database
     public function create($id, $valor)
     {
         $db = new Database;
-        $s = $db->conn->prepare("INSERT INTO dividas(cliente_id, valor) values(:id, >valor)");
+        $s = $db->conn->prepare("INSERT INTO dividas(cliente_id, valor) values(:id, :valor)");
         $s->execute(array(
             "id" => $id,
             "valor" => $valor
